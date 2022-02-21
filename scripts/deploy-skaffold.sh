007731ad1a51f4fc29a30b79469b5d9a734d1090
BasharKhouri-Voice <bkhoury@voice.com>
2022-02-21 19:34:48 +0200

    added npm install

--- scripts/deploy-skaffold.sh
+++ scripts/deploy-skaffold.sh
@@ -4,6 +4,7 @@ set -x
 
 export NO_PROXY="${KUBERNETES_SERVICE_HOST}"
 
+npm install @voice-social/voice-skaffold-tools
 # export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && nvm install v14 && npm install -g yarn retire zx 
 # yarn global add @voice-social/voice-skaffold-tools@latest
 
