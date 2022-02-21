e366aec37f784cc1bee7b89d539a6f10ca437afa
BasharKhouri-Voice <bkhoury@voice.com>
2022-02-21 19:28:28 +0200

    trace deploy.skaffold

--- scripts/deploy-skaffold.sh
+++ scripts/deploy-skaffold.sh
@@ -4,7 +4,7 @@ set -x
 
 export NO_PROXY="${KUBERNETES_SERVICE_HOST}"
 
-export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && nvm install v14 && npm install -g yarn retire zx 
-yarn global add @voice-social/voice-skaffold-tools@latest
+# export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && nvm install v14 && npm install -g yarn retire zx 
+# yarn global add @voice-social/voice-skaffold-tools@latest
 
 voice-deploy-skaffold "$@"
